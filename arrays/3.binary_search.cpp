aae08435907b5da207bed8d852d2ca4c1e5d1c36
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-18 17:35:22 +0530

    added binary_search

---
+++ arrays/3.binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int binary_search(int *arr, int n, int key){
+    int start = 0;
+    int end = n-1;
+
+    while(start <= end){
+        int mid = (start + end)/2;
+        if (arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
+int main(){
+    int arr[] = {12,34,31,25,32,45,78};
+    int n = sizeof(arr)/sizeof(int);
+    int key;
+    cin >> key;
+
+    sort(arr, arr+n);
+    int index = binary_search(arr,n,key);
+    if(index != -1){
+        cout << key << " is present at index " << index << endl;
+    }
+    else{
+        cout << key << " is not found! " <<endl;
+    }
+    return 0;
+}
