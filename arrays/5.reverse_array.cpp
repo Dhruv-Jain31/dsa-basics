6a45129cded6e206bd68600227a9620f41ca6669
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-18 18:58:37 +0530

    added reverse_array.cpp

---
+++ arrays/5.reverse_array.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void reverseArray(int *arr, int n){
+    int s = 0, e = n - 1;
+    int temp;
+    while (s < e){
+        temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+        s = s+1;
+        e = e-1;
+    }
+}
+
+int main(){
+    int arr[] = {10,20,30,45,60,80,90};
+    int n = sizeof(arr)/sizeof(int);
+
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    reverseArray(arr,n);
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
