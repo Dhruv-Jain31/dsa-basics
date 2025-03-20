2543d638c489e98d68c182f12b50dce99e325447
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-20 10:45:45 +0530

    added k rotate anticlockwise as well

--- arrays/11.k_rotate.cpp
+++ arrays/11.k_rotate.cpp
@@ -25,6 +25,16 @@ vector<int> K_rotate_clockwise(vector<int> a, int k){
     return a;
 }
 
+vector<int> K_rotate_anticlockwise(vector<int> a, int k){
+    int n = a.size();
+    k = k%n;
+
+    reverse_array(a,0,k-1);
+    reverse_array(a,k,n-1);
+    reverse_array(a,0,n-1);
+    return a;
+}
+
 int main() {
     vector<int> A = {1, 3, 5, 7, 9, 11};
 
@@ -32,13 +42,21 @@ int main() {
     int k;
     cin >> k;
 
-    vector<int> rotated = K_rotate_clockwise(A, k);
+    vector<int> rotated_clockwise = K_rotate_clockwise(A, k);
+    vector<int> rotated_anticlockwise = K_rotate_anticlockwise(A, k);
 
-    cout << "Rotated array: ";
-    for (int num : rotated) {
+    cout << "Rotated array anticlockwise: ";
+    for (int num : rotated_anticlockwise) {
         cout << num << " ";
     }
     cout << endl;
+
+    cout << "Rotated array clockwise: ";
+    for (int num : rotated_clockwise) {
+        cout << num << " ";
+    }
+    cout << endl;
+
 
     return 0;
 }
