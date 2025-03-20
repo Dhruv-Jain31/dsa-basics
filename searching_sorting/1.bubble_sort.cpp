ff4e1aa26b497ecab3f2b007afebd726c17b71a7
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-20 11:56:27 +0530

    added bubble_sort algo

---
+++ searching_sorting/1.bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void bubble_sort(int a[], int n){
+    for(int i = 1; i < n-1; i++){
+        for (int j = 0; j < n - 1 - i; j++){
+           if(a[j] > a[j+1]){
+                swap(a[j],a[j+1]);
+           }
+        }
+    }
+}
+
+int main(){
+    int arr[] ={-2,3,4,-1,5,-12,6,1,8};
+    int n = sizeof(arr)/sizeof(int);
+    bubble_sort(arr,n);
+
+    for(auto x : arr){
+        cout << x << ",";
+    }
+}
