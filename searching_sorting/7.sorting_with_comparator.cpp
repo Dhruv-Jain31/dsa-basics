a91987d69593aff7ecd6c9e6d54ca8a089c0f6de
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-29 12:47:17 +0530

    sorting exercise.cpp

---
+++ searching_sorting/7.sorting_with_comparator.cpp
@@ -0,0 +1,3 @@
+/*Given an integer vector and a bool variable flag, your task is to sort the vector in accordance to the boolean value. 
+If the bool value passed is true then sort it in non-decreasing order or vice versa.
+You can use any sorting technique of your choice.*/
