0498bf01c9b1a2b54e83cd4545ab9842ecc3c4a8
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-29 12:10:40 +0530

    added inbuilt sort and comparator.cpp

---
+++ searching_sorting/5.inbuilt_sort_and_comparator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool compare(int a, int b){
+    cout << "comparing " << a << " and " << b << endl;
+    return a > b;
+}
+
+int main(){
+    int arr[] = {10,9,8,6,5,4,3,2,11,16,8};
+    int n = sizeof(arr)/sizeof(int);
+
+    //sort(arr, arr+n, greater<int>()); does the same work as the below function
+    sort(arr, arr+ n, compare); //this is not a fn call func is passed as a parameter to another function
+    //reverse(arr, arr + n);
+
+    for(int x : arr){
+        cout << x << " ";
+    }
+    return 0;
+}
