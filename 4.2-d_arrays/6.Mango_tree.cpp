25c54c8849c6521d2ab1e6899828bb159efd72aa
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-04-10 15:18:20 +0530

    added mango tree .cpp

---
+++ 4.2-d_arrays/6.Mango_tree.cpp
@@ -0,0 +1,27 @@
+/*Ramu's father has left a farm organized as an N × N grid. Each square in the grid either has or does not have a mango tree. 
+He has to divide the farm with his three sisters as follows: he will draw one horizontal line and one vertical line to divide the field into four rectangles. 
+His sisters will choose three of the four smaller fields and he gets the last one.
+
+He wants to divide the field so that he gets the maximum number of mangos possible,
+ assuming that his sisters will pick the best three rectangles.
+
+For example, suppose the field looks as follows:
+
+      . # # . . .
+      # . . # # .
+      . # . . . .
+      . # # . . #
+      # . . # # .
+      . # . . . .
+
+Ramu can ensure that he gets at least 3 mango trees by cutting as follows:
+
+        . # | # . . .
+        # . | . # # .
+        . # | . . . .
+      ------+---------
+        . # | # . . #
+        # . | . # # .
+        . # | . . . .
+*/
+
